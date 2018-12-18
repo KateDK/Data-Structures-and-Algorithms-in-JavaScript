c5455488ada90a3083a094ceead897955c2d0101
Kate Dubitski <kate.dubitski@gmail.com>
2018-12-18 15:58:56 -0500

    priority queue setup

--- priorityQueue.js
+++ priorityQueue.js
@@ -1,1 +1,23 @@
 const { createQueue } = require('./index');
+
+function createPriorityQueue() {
+  const lowPriorityQueue = createQueue();
+  const highPriorityQueue = createQueue();
+  return {
+    enqueue(item) {
+      queue.unshift(item);
+    },
+    dequeue() {
+      queue.pop();
+    },
+    peek() {
+      return queue[queue.length - 1];
+    },
+    length() {
+      return queue.length;
+    },
+    isEmpty() {
+      return !queue.length;
+    },
+  };
+}
