5eac696bd9f5252250708d42198c3d3b8a58519f
Kate Dubitski <kate.dubitski@gmail.com>
2018-12-18 15:21:59 -0500

    can now add and remove to queue

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ function createQueue() {
   const queue = [];
   return {
     //add to queue
+    enqueue(item) {
+      queue.unshift(item);
+    },
     //remove from queue
+    dequeue() {
+      queue.pop();
+    },
     //peek
     //length
     //isEmpty
