17f0f6d293024043a39adcb64d4c732517acd915
Kate Dubitski <kate.dubitski@gmail.com>
2018-12-18 15:42:53 -0500

    queue now fully functional

--- index.js
+++ index.js
@@ -23,5 +23,20 @@ function createQueue() {
       return queue.length;
     },
     //isEmpty
+    isEmpty() {
+      return !queue.length;
+    },
   };
 }
+
+const q = createQueue();
+console.log(q.isEmpty());
+q.enqueue('Kate');
+q.enqueue('Daniel');
+q.enqueue('Sheri');
+console.log('queue length is: ', q.length());
+console.log(q.peek()); //Kate
+q.dequeue();
+console.log('after dequeueu length is: ', q.length());
+console.log('peek is: ', q.peek()); //Daniel
+console.log(q.isEmpty());
