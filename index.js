7425fcd3d52bb143847e9949ff14635604c7d066
Kate Dubitski <kate.dubitski@gmail.com>
2018-12-18 15:53:37 -0500

    code cleanup

--- index.js
+++ index.js
@@ -3,26 +3,18 @@
 function createQueue() {
   const queue = [];
   return {
-    //add to queue
     enqueue(item) {
       queue.unshift(item);
     },
-    //remove from queue
     dequeue() {
       queue.pop();
     },
-    //peek
     peek() {
       return queue[queue.length - 1];
     },
-    //length
-    /*get length(){
-      return queue.length;
-    }*/
     length() {
       return queue.length;
     },
-    //isEmpty
     isEmpty() {
       return !queue.length;
     },
