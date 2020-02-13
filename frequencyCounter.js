5a472f6220f3eaacc7316c98d6412abacf86608b
Kate Dubitski <kate.dubitski@gmail.com>
2020-02-12 19:18:58 -0500

    add solution to 'same' frequency counter function

---
+++ frequencyCounter.js
@@ -0,0 +1,16 @@
+/* Write a function called 'same'. which accepts two arrays.
+The function should return true if every value in the array has its corresponding value squared in the second array.
+The frequency of values must be the same. */
+
+function same(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    const squared = arr1.map(num => num * num);
+    arr2.sort();
+    return squared.join("") === arr2.join("");
+    }
+    return false;
+}
+
+console.log(same([1, 2, 3], [4, 1, 9])); //true
+console.log(same([1, 2, 3], [1, 9])); //false
+console.log(same([1, 2, 1], [4, 4, 1])); //false
