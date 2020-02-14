1f210f2c65325c56e847d9d8f4f018c924921279
Kate Dubitski <kate.dubitski@gmail.com>
2020-02-14 17:26:26 -0500

    Add sorting for arr2 and test case

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -4,7 +4,7 @@ The frequency of values must be the same. */
 
 function same(arr1, arr2) {
   if (arr1.length === arr2.length) {
-    const squared = arr1.map(num => num * num);
+    const squared = arr1.map(num => num * num).sort();
     arr2.sort();
     return squared.join("") === arr2.join("");
     }
@@ -12,5 +12,6 @@ function same(arr1, arr2) {
 }
 
 console.log(same([1, 2, 3], [4, 1, 9])); //true
+console.log(same([2, 1, 3], [4, 1, 9])); //true
 console.log(same([1, 2, 3], [1, 9])); //false
 console.log(same([1, 2, 1], [4, 4, 1])); //false
