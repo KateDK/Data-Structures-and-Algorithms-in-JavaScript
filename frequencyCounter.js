aeb62424dc7a0718a575d5868e79523e322c5e63
Kate Dubitski <kate.dubitski@gmail.com>
2020-02-14 18:04:56 -0500

    remove sort logic to improve preformance

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -4,9 +4,31 @@ The frequency of values must be the same. */
 
 function same(arr1, arr2) {
   if (arr1.length === arr2.length) {
-    const squared = arr1.map(num => num * num).sort();
-    arr2.sort();
-    return squared.join("") === arr2.join("");
+    const obj1={};
+    const obj2={};
+    for(let i = 0; i < arr1.length; i++){
+      const squared = arr1[i]*arr1[i];
+      const compareTo = arr2[i];
+      if(obj1[squared]){
+        obj1[squared]++;
+      }else{
+        obj1[squared] = 1;
+      }
+      if(obj2[compareTo]){
+        obj2[compareTo]++;
+      }else{
+        obj2[compareTo] = 1;
+
+      }
+    }
+    const compare = Object.keys(obj1);
+    for(let i = 0; i < compare.length; i++){
+      const currentItem = compare[i];
+      if(obj2[currentItem] !== obj1[currentItem]){
+        return false;
+      }
+    }
+    return true;
     }
     return false;
 }
