1e2fd013e8cdcb76c2bf67cc737a03216efa630f
Kate Dubitski <kate.dubitski@gmail.com>
2020-03-03 16:30:32 -0500

    add another test case

--- multiplePointers.js
+++ multiplePointers.js
@@ -22,6 +22,7 @@ function sumZero(arr){
 }
 
 console.log(sumZero([-3,-2,-1,0,1,2,3]));//[-3,3];
+console.log(sumZero([-3,-2,-1,0,1,2,3,4]));//[-3,3];
 console.log(sumZero([-2,0,1,3]));//undefined
 console.log(sumZero([1,2,3]));//undefined
 console.log(sumZero([]));//undefined
