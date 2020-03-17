1f97ede67bbb75b371d30115d527576a4b5685bb
Kate Dubitski <kate.dubitski@gmail.com>
2020-03-17 14:43:44 -0400

    add countUniqueValues solution with one pointer

--- multiplePointers.js
+++ multiplePointers.js
@@ -35,3 +35,21 @@ console.log(sumZero([]));//undefined
   implement a function called countUniqueValues, which takes a sorted array and counts the unique values in the array.
   There can be negative numbers in the array, but it will always be sorted.
  */
+
+const countUniqueValues = (arr) => {
+  const map = {};
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i];
+    if (!map[current]) {
+      map[current] = true;
+      count++;
+    }
+  }
+  return count;
+};
+
+ console.log(countUniqueValues([1,1,1,1,1,2])); //2
+ console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])); //7
+ console.log(countUniqueValues([])); //0
+ console.log(countUniqueValues([-2,-1,-1,0,1])); //4
