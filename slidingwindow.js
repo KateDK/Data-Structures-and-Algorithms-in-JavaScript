64d159c8e08186b9c35bee9d4a0aca282848e09d
Kate Dubitski <kate.dubitski@gmail.com>
2020-04-09 18:19:08 -0400

    Add slidingwindow file and problem definition

---
+++ slidingwindow.js
@@ -0,0 +1,4 @@
+/**
+ Write a function called maxSubarraySum which takes an array of integers and number called n.
+ the function should calculate the maximum sum of n consecutive elements in the array.
+ */
