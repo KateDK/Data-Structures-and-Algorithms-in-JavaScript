330d6b46059de12a71510517d13cd7c7fd445a1e
Kate Dubitski <kate.dubitski@gmail.com>
2020-04-14 20:05:27 -0400

    add test cases
    -add empty function deffinition

--- slidingwindow.js
+++ slidingwindow.js
@@ -2,3 +2,13 @@
  Write a function called maxSubarraySum which takes an array of integers and number called n.
  the function should calculate the maximum sum of n consecutive elements in the array.
  */
+
+ const maxSubarraySum = (arr, n)=>{
+
+ };
+
+console.log(maxSubarraySum([1,2,3,4,5,6],2));//11
+console.log(maxSubarraySum([1,2,3,4,5,6],3));//15
+console.log(maxSubarraySum([2,2,2,2],3));//6
+console.log(maxSubarraySum([],5));//0
+console.log(maxSubarraySum([1,1,1,1],6));//4
