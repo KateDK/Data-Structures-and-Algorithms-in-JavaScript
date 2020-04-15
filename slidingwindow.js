14bac04b89f1f76170be1e29de6ead42f7a7379b
Kate Dubitski <kate.dubitski@gmail.com>
2020-04-14 20:07:39 -0400

    update test cases

--- slidingwindow.js
+++ slidingwindow.js
@@ -10,5 +10,5 @@
 console.log(maxSubarraySum([1,2,3,4,5,6],2));//11
 console.log(maxSubarraySum([1,2,3,4,5,6],3));//15
 console.log(maxSubarraySum([2,2,2,2],3));//6
-console.log(maxSubarraySum([],5));//0
+console.log(maxSubarraySum([],5));//null
 console.log(maxSubarraySum([1,1,1,1],6));//4
