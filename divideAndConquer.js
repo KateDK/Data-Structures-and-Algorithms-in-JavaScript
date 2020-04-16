86268a23283c795539fd3ede202264d466e3588b
Kate Dubitski <kate.dubitski@gmail.com>
2020-04-16 04:20:51 -0400

    add divide and conquer file with problem definition

---
+++ divideAndConquer.js
@@ -0,0 +1,4 @@
+/**
+ Write a function called search that takes a sorted array and a value, and returns the index where the value is located in the array.
+ If the value is not found in the array the function should return -1.
+ */
