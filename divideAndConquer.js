6d2e3782d44ba7666d60ef436dcf3237b0969d1c
Kate Dubitski <kate.dubitski@gmail.com>
2020-04-16 04:24:03 -0400

    added test cases and empty func def

--- divideAndConquer.js
+++ divideAndConquer.js
@@ -2,3 +2,11 @@
  Write a function called search that takes a sorted array and a value, and returns the index where the value is located in the array.
  If the value is not found in the array the function should return -1.
  */
+
+ const search = (arr, val)=>{};
+
+ console.log(search([1,2,3,4,5,6],4));//3
+ console.log(search([1,2,3,4,5,6],6));//5
+ console.log(search([1,2,3,4,5,6],11));//-1
+
+
