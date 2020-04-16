b238933b69fb9aeb7e431f0c75e43c51395828c3
Kate Dubitski <kate.dubitski@gmail.com>
2020-04-16 17:02:38 -0400

    add solution to problem

--- divideAndConquer.js
+++ divideAndConquer.js
@@ -3,16 +3,39 @@
  If the value is not found in the array the function should return -1.
  */
 
- const search = (arr, val)=>{};
+const search = (arr, val) => {
+  let index = Math.floor(arr.length / 2);
+  let visited = {};
+  while (index >= 0 && index < arr.length) {
+    const current = arr[index];
 
- console.log(search([1,2,3,4,5,6],4));//3
- console.log(search([1,2,3,4,5,6],6));//5
- console.log(search([1,2,3,4,5,6],11));//-1
- console.log(search([],11));//-1
- console.log(search([1,3,5,6,8,9],7));//-1
- console.log(search([1, 3, 5, 6, 8, 9], 7));//-1
- console.log(search([1,2,3,4,5,6,6],6));//5
- console.log(search([1,1,2,3,4,5,6,6],1));//0
+    if (visited[index]) {
+      return -1;
+    } else {
+      visited[index] = true;
+
+      if (current === val) {
+        return index;
+      }
+      if (current > val) {
+        index--;
+      } else {
+        index++;
+      }
+    }
+  }
+  return -1;
+};
+
+console.log(search([1,2,3,4,5,6],4));//3
+console.log(search([1,2,3,4,5,6],6));//5
+console.log(search([1,2,3,4,5,6],11));//-1
+console.log(search([],11));//-1
+console.log(search([1,3,5,6,8,9],7));//-1
+console.log(search([1, 3, 5, 6, 8, 9], 7));//-1
+console.log(search([1,2,3,4,5,6,6],6));//5
+console.log(search([1,1,2,3,4,5,6,6],1));//0
+
 
 
 
