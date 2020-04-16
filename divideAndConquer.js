ec06bee1262337d9111fca0b166b191f05906bcb
Kate Dubitski <kate.dubitski@gmail.com>
2020-04-16 16:13:51 -0400

    add test cases

--- divideAndConquer.js
+++ divideAndConquer.js
@@ -8,5 +8,8 @@
  console.log(search([1,2,3,4,5,6],4));//3
  console.log(search([1,2,3,4,5,6],6));//5
  console.log(search([1,2,3,4,5,6],11));//-1
+ console.log(search([],11));//-1
+ console.log(search([1,3,5,6,8,9],7));//-1
+
 
 
