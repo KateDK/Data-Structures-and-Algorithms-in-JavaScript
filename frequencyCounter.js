9966039ff36efde94d894081729c24f2722719b7
Kate Dubitski <kate.dubitski@gmail.com>
2020-04-20 15:41:16 -0400

    add test case

--- frequencyCounter.js
+++ frequencyCounter.js
@@ -132,3 +132,4 @@ return false;
 console.log(areThereDuplicates(1,2,3));//false
 console.log(areThereDuplicates(1,2,2));//true
 console.log(areThereDuplicates("a", "b", "c", "a"));//true
+console.log(areThereDuplicates());//false
