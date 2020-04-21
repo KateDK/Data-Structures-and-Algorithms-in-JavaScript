df067cc08e5a41ed60b638982d07bae0cfeda9c0
Kate Dubitski <kate.dubitski@gmail.com>
2020-04-21 15:13:14 -0400

    add empty minSubArray function declaration
    -add test cases

--- slidingwindow.js
+++ slidingwindow.js
@@ -51,3 +51,13 @@ console.log(maxSubarraySum([1,2,5,2,8,1,5],4));//17
 Write a function called minSubArray that takes an array of positive integers and a positive integer
 The function should return the minimal length of contiguous subarray of which the sum is greater or equal to the integer passed to the function. The function should return 0 if none exsist.
 */
+
+const minSubArray = (arr, num) => {};
+
+console.log(minSubArray([2,3,1,2,4,3],7));//2
+console.log(minSubArray([2,1,6,5,4],9));//2
+console.log(minSubArray([3,1,7,11,2,9,8,21,62,33,19],));//1
+console.log(minSubArray([1,4,16,22,5,7,8,9,10],39));//3
+console.log(minSubArray([1,4,16,22,5,7,8,9,10],55));//5
+console.log(minSubArray([4,3,3,8,1,2,3],11));//0
+console.log(minSubArray([1,4,16,22,5,7,8,9,10],95));//0
