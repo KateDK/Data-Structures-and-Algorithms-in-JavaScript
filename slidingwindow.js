e20d2984cae289419e3977b2c251c8320ea85f9b
Kate Dubitski <kate.dubitski@gmail.com>
2020-04-24 14:51:19 -0400

    fix and add tests for minSubArray

--- slidingwindow.js
+++ slidingwindow.js
@@ -56,11 +56,13 @@ const minSubArray = (arr, num) => {};
 
 console.log(minSubArray([2,3,1,2,4,3],7));//2
 console.log(minSubArray([2,1,6,5,4],9));//2
-console.log(minSubArray([3,1,7,11,2,9,8,21,62,33,19],));//1
+console.log(minSubArray([3,1,7,11,2,9,8,21,62,33,19],52));//1
 console.log(minSubArray([1,4,16,22,5,7,8,9,10],39));//3
 console.log(minSubArray([1,4,16,22,5,7,8,9,10],55));//5
 console.log(minSubArray([4,3,3,8,1,2,3],11));//0
 console.log(minSubArray([1,4,16,22,5,7,8,9,10],95));//0
+console.log(minSubArray([],1));//0
+console.log(minSubArray([1,2,3,4],3));//1
 
 
 /*
