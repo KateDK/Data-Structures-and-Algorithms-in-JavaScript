c19314bb7f6e02e67f7f1439379dc627cfd16f07
Kate Dubitski <kate.dubitski@gmail.com>
2020-04-27 17:57:22 -0400

    fix test case

--- slidingwindow.js
+++ slidingwindow.js
@@ -59,7 +59,7 @@ console.log(minSubArray([2,1,6,5,4],9));//2
 console.log(minSubArray([3,1,7,11,2,9,8,21,62,33,19],52));//1
 console.log(minSubArray([1,4,16,22,5,7,8,9,10],39));//3
 console.log(minSubArray([1,4,16,22,5,7,8,9,10],55));//5
-console.log(minSubArray([4,3,3,8,1,2,3],11));//0
+console.log(minSubArray([4,3,3,8,1,2,3],11));//2
 console.log(minSubArray([1,4,16,22,5,7,8,9,10],95));//0
 console.log(minSubArray([],1));//0
 console.log(minSubArray([1,2,3,4],3));//1
