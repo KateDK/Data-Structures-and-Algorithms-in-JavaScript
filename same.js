381430f902625056d53a1c0194a5a213370d14bc
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-15 14:26:31 -0400

    Add 'same' problem description

---
+++ same.js
@@ -0,0 +1,10 @@
+/**
+ * Write a function called same, which accepts two arrays.
+ * The function should return true if every value in the first array has its corresponding value squared in the second array. The frequency of values must be the same.
+ */
+
+ function same(arr1,arr2){
+
+ }
+
+ console.log(same([],[]));//
