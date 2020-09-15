690f96e880a2a5557195f90b3e22ef84d429f508
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-15 14:21:35 -0400

    Add problem solving approach doc

---
+++ approach.js
@@ -0,0 +1,45 @@
+/*
+~~ Understand the problem ~~
+----------------------------
+1. Can I repeat the problem in my own words?
+2. What are the inputs that go into the probkem?
+3. What are the outputs expected from the solution to the problem?
+4. Can the outputs be determined by the inputs?
+5. How should the importent pieces of data be labled?
+*/
+
+/*
+~~ Make Examples ~~
+--------------------
+1. Start with simple examples
+2. progress to more complex examples
+3. Explore examples with empty inputs
+4. Explore examples with invalid inputs
+*/
+
+/*
+~~ Break it down ~~
+--------------------
+Think about the steps needed to solve the problem
+*/
+
+/*
+~~ Solve or simplify ~~
+-----------------------
+Splve the problem. If you cant - solve a simpler problem:
+- Find the core dificulty in what you're trying to do.
+- Break it down to smaller problems
+OR
+- Temporarily ignore that dificulty
+- Write a simplified solution
+- Incorprate the dificulkty back in
+*/
+
+/*
+~~ Refactor ~~
+--------------
+Can we improve our solution?
+- Is the solution readable?
+- Can the preformance be improved?
+- How did others solve this problem?
+*/
