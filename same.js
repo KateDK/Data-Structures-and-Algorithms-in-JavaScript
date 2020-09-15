d15275338a4047c4943c85864c055ade2771a2ec
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-15 15:34:55 -0400

    Refactor solution to O(n)

--- same.js
+++ same.js
@@ -3,12 +3,37 @@
  * The function should return true if every value in the first array has its corresponding value squared in the second array. The frequency of values must be the same.
  */
 
- function same(arr1,arr2){
+//  function same(arr1,arr2){
+//   if(arr1.length !== arr2.length) return false;
+//   for(let i = 0; i < arr1.length; i++){
+//     const sqrIndex = arr2.indexOf(arr1[i]**2);
+//     sqrIndex === -1 ? false : arr2.splice(sqrIndex,1);
+//   }
+//   return true;
+//  }
+
+function same(arr1,arr2){
   if(arr1.length !== arr2.length) return false;
-  for(let i = 0; i < arr1.length; i++){
-    const sqrIndex = arr2.indexOf(arr1[i]**2);
-    sqrIndex === -1 ? false : arr2.splice(sqrIndex,1);
-  }
+  const map1 = {};
+  const map2 = {};
+
+  arr1.map(elm=>{
+    map1[elm] ? map1[elm]++ : map1[elm]=1;
+  });
+
+  arr2.map(elm=>{
+    map2[elm] ? map2[elm]++ : map2[elm]=1;
+  });
+
+  for(let key in map1){
+    const sqr = key**2;
+    if(!map2[sqr]){
+      return false;
+    };
+    if(map2[sqr] !== map1[key]){
+      return false;
+    };
+  };
   return true;
  }
 
