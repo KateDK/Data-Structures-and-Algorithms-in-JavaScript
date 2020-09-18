b3d7c0a7bf86a90a1f53ff410d458421a5fdeb26
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-18 16:26:49 -0400

    fix sumZero test cases

--- sumZero.js
+++ sumZero.js
@@ -12,8 +12,8 @@
  console.log(sumZero([-2,0,1,3]));//undefind
  console.log(sumZero([1,2,3]));//undefined
  console.log(sumZero([0,0,1,2,3]));//[0,0]
- console.log(sumZero([-3,0,0,1,2,3]));//[-3,3]????
- console.log(sumZero([-3,-2,-1,0,0,1]));//[0,0]
+ console.log(sumZero([-3,0,0,1,2,3]));//[-3,3]
+ console.log(sumZero([-3,-2,-1,0,0,1]));//[-1,1]
  console.log(sumZero([]));//undefined
  console.log(sumZero([0]));//undefined
  console.log(sumZero([2]));//undefined
