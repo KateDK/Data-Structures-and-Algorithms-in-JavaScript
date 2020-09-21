8f7bc5d6b6fea5a96d767c22136037fdf5a65b00
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-21 16:11:21 -0400

    Add maxSubArraySum file and problem description
    -add maxSubArraySum tests

---
+++ maxSubArraySum.js
@@ -0,0 +1,10 @@
+/**
+ * write a function called maxSubArraySum which takes an array of integers and a number called n.
+ * The function should calculate the maximum sum of n consecutive elements in the array.
+ */
+
+ console.log(maxSubArraySum([1,2,5,2,8,1,5],2));//10
+ console.log(maxSubArraySum([1,2,5,2,8,1,5],4));//17
+ console.log(maxSubArraySum([4,2,1,6],1));//6
+ console.log(maxSubArraySum([4,2,1,6,2],4));//13
+ console.log(maxSubArraySum([],4));//null
