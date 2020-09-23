b4fb74ce2b6ace9cb16badbdf34a997160c59cbf
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-23 15:00:35 -0400

    Add additional search test

--- search.js
+++ search.js
@@ -20,4 +20,5 @@ const search = (arr,val) =>{
  console.log(search([1,2,3,4,5,6],4));//3
  console.log(search([1,2,3,4,5,6],6));//5
  console.log(search([1,2,3,4,5,6],11));//-1
+ console.log(search([1,2,3,5,6],4));//-1
  console.log(search([],2));//-1
