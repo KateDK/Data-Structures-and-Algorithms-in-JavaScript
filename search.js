9f76db10ecbe9b75a4ef04979a955c5e7203dd18
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-23 14:53:20 -0400

    Add search file and problem definition

---
+++ search.js
@@ -0,0 +1,12 @@
+/**
+ * Given a sorted array of integers, write a function called search that accepts a value and returns the index where the value is located in the array.
+ * if value is not found return -1
+ */
+const search = () =>{
+
+};
+
+ console.log(search([1,2,3,4,5,6],4));//3
+ console.log(search([1,2,3,4,5,6],6));//5
+ console.log(search([1,2,3,4,5,6],11));//-1
+ console.log(search([],2));//-1
