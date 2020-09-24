31b94d4620e7a2b5733d76b10b13ad265ccb4e1d
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-24 12:56:56 -0400

    Add sameFrequency file and problem description

---
+++ sameFrequency.js
@@ -0,0 +1,11 @@
+/**
+ * Write a function called sameFrequency.
+ * Given two positive integers, find out if the two numbers have the same frequency of digits.
+ * Time complexity should be O(n)
+ */
+
+
+ console.log(sameFrequency(182,281));//true
+ console.log(sameFrequency(34,14));//false
+ console.log(sameFrequency(3589578,5879385));//true
+ console.log(sameFrequency(22,222));//false
