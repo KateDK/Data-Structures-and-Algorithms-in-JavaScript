1292757363fb300fb5b37014efd14b8a1fd5db45
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-29 10:32:37 -0400

    add more averagePair test cases

--- averagePair.js
+++ averagePair.js
@@ -10,3 +10,5 @@
  console.log(averagePair([13,3,5,6,7,10,12,19],8));//true
  console.log(averagePair([-1,0,3,4,5,6],4.1));//false
  console.log(averagePair([],4));//false;
+ console.log(averagePair([3],4));//false;
+ console.log(averagePair([4],4));//true;
