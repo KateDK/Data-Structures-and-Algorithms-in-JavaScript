e3cec232077da137fedbc987d0e2a1fb81de4fa5
Kate Dubitski <kate.dubitski@gmail.com>
2020-09-29 10:43:01 -0400

    fix averagePair test case

--- averagePair.js
+++ averagePair.js
@@ -7,7 +7,7 @@
  const averagePair = (arr,avg)=>{};
 
  console.log(averagePair([1,2,3],2.5));//true
- console.log(averagePair([13,3,5,6,7,10,12,19],8));//true
+ console.log(averagePair([1,3,3,5,6,7,10,12,19],8));//true
  console.log(averagePair([-1,0,3,4,5,6],4.1));//false
  console.log(averagePair([],4));//false;
  console.log(averagePair([3],4));//false;
