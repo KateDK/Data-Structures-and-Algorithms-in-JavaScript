2cd31dc0a67581faa4d9aa031bf26cc0ce087a87
Kate Dubitski <kate.dubitski@gmail.com>
2020-10-01 14:52:59 -0400

    Add Shuffle problem description and test cases

---
+++ arrayShuffle.js
@@ -0,0 +1,9 @@
+/**
+ * Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn]
+ */
+
+ console.log(shuffle([2,5,1,3,4,7]),6);//[2,3,5,4,1,7]
+ console.log(shuffle([1,2,3,4,4,3,2,1]),4);//[1,4,2,3,3,2,4,1]
+ console.log(shuffle([1,1,2,2]),2);//[1,2,1,2]
