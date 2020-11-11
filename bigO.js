07a1c5531e3055666a60a9630888afd4c13cbe5b
Kate Dubitski <kate.dubitski@gmail.com>
2020-11-10 19:19:14 -0500

    Add bigO doc

---
+++ bigO.js
@@ -0,0 +1,9 @@
+/**
+ * Big O is a system that allows us to evaluate fow efficient our code is (in terms of run time or space).
+ *
+ * Why is big O important:
+ * - Having a unified language on performance helps us understand and communicate to others about our code.
+ * - Big O can be helpful when thinking about trade-offs about our code.
+ * - Can help us identify pain points in our code.
+ * - Big O can come up in interviews
+ */
