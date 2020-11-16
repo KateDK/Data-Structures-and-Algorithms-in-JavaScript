a60aadbda756bcda6a7dc43454feae75fb605a1b
Kate Dubitski <kate.dubitski@gmail.com>
2020-11-16 14:07:39 -0500

    Solve "same" problem

--- same.js
+++ same.js
@@ -42,3 +42,43 @@
 //  console.log(same([1],[1]));//true
 //  console.log(same([2],[4,4]));//false
 //  console.log(same([2,2],[4,4]));//true
+
+const same = (arr1, arr2)=>{
+  //check arrays equal in length;
+  if(arr1.length === arr2.length){
+    //create map obj
+    const mapObj = {};
+    //loop over arr1
+    for(let i = 0; i < arr2.length; i++){
+      const elm = arr1[i]*arr1[i];
+      //square every element
+      mapObj[elm] ? mapObj[elm]++ : mapObj[elm]=1;
+      //store every squared element in map obj
+
+    }
+    //loop over arr2 from the end
+    let i = arr2.length-1;
+    while(i >=0){
+      const current = arr2[i];
+      if(mapObj[current] > 0){
+        mapObj[current]--;
+        arr2.pop();
+      }else{
+        return false;
+      }
+      //check if element in map obj
+      //pop last element from arr1
+      i--;
+    }
+    return true;
+  }else{
+    return false;
+  }
+};
+
+console.log(same([1,2],[1,4]));//true
+console.log(same([3,3,2,1],[9,4,9,1]));//true
+console.log(same([3,3,2,1],[9,9,9,1]));//false
+console.log(same([3,3,2,1],[9,9,4]));//false
+console.log(same([3,3,2,1],[9,9,4,1,4]));//false
+console.log(same([],[]));//true
