4c0f95f827153237fae0dbb9089301d95edeb98c
Kate Dubitski <kate.dubitski@gmail.com>
2020-11-17 16:02:23 -0500

    Add new solution

--- validAnagram.js
+++ validAnagram.js
@@ -24,9 +24,30 @@
 
 //   return true;
 // };
-    }
-  }
 
+
+const validAnagram = (str1,str2)=>{
+  if(str1.length === str2.length){
+    //create map of str1,str2
+    const map1 ={};
+    const map2 ={};
+    //loop over str1 and populate elements in map;
+    for(let i = 0; i < str1.length; i++){
+      const current = str1[i];
+      map1[current] ? map1[current]++ : map1[current]=1;
+    }
+    //loop over str2 and populate in map
+    for(let i = 0; i < str2.length; i++){
+      const current = str2[i];
+      map2[current] ? map2[current]++ : map2[current]=1;
+    }
+    //loop over one map and compare to another
+    for(key in map1){
+      if(map1[key] !== map2[key])return false;
+    }
+    return true;
+  }
+  return false;
 };
 
 console.log(validAnagram("",""));//true
